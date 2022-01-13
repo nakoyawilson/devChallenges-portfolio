78bd00fd1c5c39d714111d923e561c3f9a39f57e
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-01-13 09:38:25 -0400

    Update data and add project screenshots

--- src/App.js
+++ src/App.js
@@ -129,10 +129,10 @@ const App = () => {
             return (
               <ProjectCard
                 key={item.id}
-                projectTitle="Recipe Blog"
+                projectTitle={item.projectTitle}
                 paragraphContent={item.description}
                 projectTags={item.tags}
-                imageSrc="https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80"
+                imageSrc={item.screenshot}
                 demoLink={item.demoLink}
                 codeLink={item.codeLink}
                 cardOrientation="vertical"
