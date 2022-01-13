57f944f6b89dfb01cf3d58ba649d847375304a55
Nakoya Wilson <wilson.nakoya@gmail.com>
2022-01-13 14:10:03 -0400

    Make CSS changes and replace gaming picture

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -186,7 +186,9 @@ export const hobbiesArray = [
     id: 1,
     hobby: "Gaming",
     description: "Quisque feugiat malesuada molestie.",
-    image: "/assets/images/gaming.jpg",
+    // image: "/assets/images/gaming.jpg",
+    image:
+      "https://images.unsplash.com/photo-1627855437629-62b298df7542?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1282&q=80",
   },
   {
     id: 2,
